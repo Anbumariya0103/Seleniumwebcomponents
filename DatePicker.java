7bacd7de031b5fb84f0b3f6c739eb0f5f2c66082
Anbumariya0103 <145959705+Anbumariya0103@users.noreply.github.com>
2023-10-30 22:48:48 +0530

    Update DatePicker.java

--- DatePicker.java
+++ DatePicker.java
@@ -11,15 +11,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DatePicker {
 
 	public static void main(String[] args) throws InterruptedException {
+		// //Initialization and Launch the browser
 WebDriver driver=new ChromeDriver();
+		//  Load the respective URL
 driver.navigate().to("https://jqueryui.com/datepicker/");
+		// this step is used too manage the window to maximize
 driver.manage().window().maximize();
+		//wait is added to prevent from the exception
 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// switch to frame to access the datepicker
 driver.switchTo().frame(0);
+		////Located the textbox by using the locator xpath and then click action is done
 driver.findElement(By.xpath("//input[@class='hasDatepicker']")).click();
+		// Located the calender and clicked on next arrow 
 driver.findElement(By.xpath("//span[text()='Next']")).click();
+		// Located the respective date
   WebElement date=driver.findElement(By.xpath("//a[@data-date='22']"));
+		// clicked the respective date
   date.click();
+		// printed the date on the  console
  System.out.println(date.getText());
 
 	}
