df5a0ff885582f82e295476d2deff9a844d41a0a
Anbumariya0103 <145959705+Anbumariya0103@users.noreply.github.com>
2023-11-01 17:18:31 +0530

    Updated NestedFrames.java

--- NestedFrames.java
+++ NestedFrames.java
@@ -13,30 +13,37 @@ public class NestedFrames {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		driver.navigate().to("https://the-internet.herokuapp.com/nested_frames");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		 driver.switchTo().frame(driver.findElement(By.name("frame-top")));
-		 List<WebElement> frames1=driver.findElements(By.tagName("frame"));
-		 System.out.println( "No.of frames on the page:"+frames1.size());
-	 WebElement ele=driver.findElement(By.xpath("//frame[@name=\"frame-left\"]"));
-	 driver.switchTo().frame(ele);
-	 WebElement body = driver.findElement(By.cssSelector("body"));
-		System.out.println(body.getText());
-		// driver.switchTo().frame(driver.findElement(By.name("frame-top")));
-	//Thread.sleep(3000);
-	WebElement ele1=driver.findElement(By.xpath("//frame[@name=\"frame-middle\"]"));
-	driver.switchTo().frame(ele1);
-	WebElement body1 = driver.findElement(By.cssSelector("body"));
-	System.out.println(body1.getText());
-	WebElement ele2=driver.findElement(By.xpath("//frame[@name=\"frame-right\"]"));
-    driver.switchTo().frame(ele2);
-    WebElement body2 = driver.findElement(By.cssSelector("body"));
-	System.out.println(body2.getText());
-	WebElement ele3=driver.findElement(By.xpath("//frame[@name=\"frame-bottom\"]"));
-	driver.switchTo().frame(ele3);
-	WebElement body3 = driver.findElement(By.cssSelector("body"));
-	System.out.println(body3.getText());
+	   driver.manage().window().maximize();
+	   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	   driver.switchTo().frame(driver.findElement(By.name("frame-top")));
+	   List<WebElement> frames1=driver.findElements(By.tagName("frame"));
+	   System.out.println( "No.of frames on the page:"+frames1.size());
+	   WebElement ele=driver.findElement(By.xpath("//frame[@name=\"frame-left\"]"));
+	   driver.switchTo().frame(ele);
+	   WebElement body = driver.findElement(By.cssSelector("body"));
+	   System.out.println(body.getText());
+	   driver.switchTo().parentFrame();
+	   WebElement ele1=driver.findElement(By.xpath("//frame[@name=\"frame-middle\"]"));
+	   driver.switchTo().frame(ele1);
+       WebElement body1 = driver.findElement(By.cssSelector("body"));
+	   System.out.println(body1.getText());
+	   driver.switchTo().parentFrame();
+	   WebElement ele2=driver.findElement(By.xpath("//frame[@name=\"frame-right\"]"));
+       driver.switchTo().frame(ele2);
+       WebElement body2 = driver.findElement(By.cssSelector("body"));
+       System.out.println(body2.getText());
+       driver.switchTo().parentFrame();
+       driver.switchTo().defaultContent();
+	   WebElement ele3=driver.findElement(By.xpath("//frame[@name=\"frame-bottom\"]"));
+	   driver.switchTo().frame(ele3);
+       WebElement body3 = driver.findElement(By.cssSelector("body"));
+	   System.out.println(body3.getText());
+	   driver.switchTo().parentFrame();
+	   driver.switchTo().defaultContent();
+	   String s=driver.getTitle();
+	   System.out.println(s);
+	   driver.close();
 	}
 
 }
-	 	 
+	 	 	 	 
